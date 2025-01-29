0dbf2a3d846a47fd576a5c3e687cdf6265df2e25
yaroslav-korol <18688588+yaroslav-korol@users.noreply.github.com>
2025-01-29 17:19:18 +0000

    Implemented selection sort

--- DSA.py
+++ DSA.py
@@ -1,5 +1,6 @@
 # BINARY SEARCH
 
+
 def binary_search(iter_object: list, target: any):
     print("Enter binary search")
     low: int = 0
@@ -12,25 +13,46 @@ def binary_search(iter_object: list, target: any):
         current_guess: any = iter_object[middle]
 
         # debug
-        print(f"Debug values")
+        print("Debug values")
         print(f"Middle: {middle}, Current guess: {current_guess}")
 
         if current_guess == target:
             return middle
-        
+
         if current_guess < target:
             low = middle
-        
+
         if current_guess > target:
             high = middle
 
     return None
-    
 
 
-nums = [1, 3, 7, 11, 21, 22, 23, 34, 111, 123, 345, 456, 489, 666, 999, 1001, 1234, 2334, 67567, 569056]
+def selection_sort(unsorted_list: list) -> list:
+    list_len: int = len(unsorted_list) - 1
 
-target_object = 2334
-result = binary_search(nums, target_object)
+    for i in range(list_len):
+        for j in range(i + 1, list_len + 1):
+            current_value: int = unsorted_list[j]
+            if current_value < unsorted_list[i]:
+                unsorted_list[j] = unsorted_list[i]
+                unsorted_list[i] = current_value
 
-print(result)
+    return unsorted_list
+
+
+def main():
+    unordered_nums: list[int] = [7, 3, 1, 11, 23, 34, 345, 123, 456, 89, 1001, 134, 567, 5656]
+
+    ordered_nums: list[int] = selection_sort(unordered_nums)
+    print(ordered_nums)
+
+    target_object = 2334
+    # target_object = 11
+    result = binary_search(ordered_nums, target_object)
+
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
