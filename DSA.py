0b8c38c08bce13dd748d976a0d9febb690717322
yaroslav-korol <18688588+yaroslav-korol@users.noreply.github.com>
2025-02-13 16:46:51 +0000

    DSA. Added introductional theoretical notes

--- DSA.py
+++ DSA.py
@@ -1,3 +1,31 @@
+# LOGARITHMS
+
+# log10 100 is like asking, “How many 10s do we multiply together to get 100?”
+# The answer is 2: 10 × 10. So log10 100 = 2.
+# Logs are the flip of exponentials.
+
+# When we talk about running time in Big O notation, log always means log2.
+
+# When you search for an element using simple search,
+# in the worst case you might have to look at every single element.
+# So for a list of 8 numbers, you’d have to check 8 numbers at most.
+
+# For binary search, you have to check log n elements in the worst case.
+# For a list of 8 elements, log 8 == 3, because 23 == 8.
+# So for a list of 8 numbers, you would have to check 3 numbers at most.
+# For a list of 1,024 elements, log 1,024 = 10, because 210 == 1,024.
+# So for a list of 1,024 numbers, you’d have to check 10 numbers at most.
+
+
+# ALGORITHM RUN TIME. BIG O NOTATION
+
+# Algorithm speed isn’t measured in seconds, but in growth of the number of operations.
+# • Instead, we talk about how quickly the run time of an algorithm increases as the size of the input increases.
+# • Run time of algorithms is expressed in Big O notation.
+# • Binary search is a lot faster than simple search.
+# • O(log n) is faster than O(n), but it gets a lot faster once the list of items you’re searching through grows.
+
+
 # BINARY SEARCH
 
 
@@ -9,7 +37,7 @@ def binary_search(iter_object: list, target: any):
     print(f"Low: {low}, High: {high}")
 
     while low <= high:
-        middle: int = int((high + low) / 2)
+        middle: int = int((high + low) / 2)  # bug here
         current_guess: any = iter_object[middle]
 
         # debug
