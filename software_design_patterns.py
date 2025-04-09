54c928ded7946a08ee769c13dcafc14e21036aff
yaroslav-korol <18688588+yaroslav-korol@users.noreply.github.com>
2025-04-09 15:54:31 +0100

    Software Design Patterns. Implemented: Singleton

--- software_design_patterns.py
+++ software_design_patterns.py
@@ -3,7 +3,7 @@
 
 # CREATIONAL PATTERNS:
 
-# Factory Method
+# FACTORY METHOD
 #   The Factory Method is a creational design pattern that provides an interface for creating objects
 #   in a superclass but allows subclasses to alter the type of objects that will be created.
 from abc import ABC, abstractmethod
@@ -52,31 +52,64 @@ class TruckFactory(VehicleFactory):
 
 
 # TEST CASE
-carFactory: VehicleFactory = CarFactory()
-truckFactory: VehicleFactory = TruckFactory()
-bikeFactory: VehicleFactory = BikeFactory()
+# carFactory: VehicleFactory = CarFactory()
+# truckFactory: VehicleFactory = TruckFactory()
+# bikeFactory: VehicleFactory = BikeFactory()
 
-myCar: Vehicle = carFactory.createVehicle()
-myTruck: Vehicle = truckFactory.createVehicle()
-myBike: Vehicle = bikeFactory.createVehicle()
+# myCar: Vehicle = carFactory.createVehicle()
+# myTruck: Vehicle = truckFactory.createVehicle()
+# myBike: Vehicle = bikeFactory.createVehicle()
 
-print(myCar.getType())  #    "Car"
-print(myTruck.getType())  #  "Truck"
-print(myBike.getType())  #   "Bike"
+# print(myCar.getType())  #    "Car"
+# print(myTruck.getType())  #  "Truck"
+# print(myBike.getType())  #   "Bike"
 
 
-# Builder
+# SINGLETON
+class Singleton:
+    _is_instance = None
 
-# Singleton
+    # Re-writing built-in __new__ method
+    def __new__(cls):
+        if not cls._is_instance:
+            cls._is_instance = super(Singleton, cls).__new__(cls)
+            cls._is_instance._init_value()
+
+        return cls._is_instance
+
+    def _init_value(self):
+        self.value = "default"
+
+    def getValue(self) -> str:
+        return self.value
+
+    def setValue(self, value: str):
+        self.value = value
+
+
+# TEST CASES
+first_instance: Singleton = Singleton()
+print(f"Default 'First instance' value is: {first_instance.getValue()}")
+first_instance.setValue(value="modified first instance value")
+print(f"New 'First instance' value is: {first_instance.getValue()}")
+
+second_instance: Singleton = Singleton()
+print(f"Second instance value is: {second_instance.getValue()}")
+
+print(first_instance is second_instance)  # True
+
+
+# BUILDER
 
 
 # STRUCTURAL PATTERNS:
 
-# Facade
-# Adapter
-# Decorator
+# FACADE
+# ADAPTER
+# DECORATOR
 
 
 # BEHAVIORAL PATTERNS:
-# Strategy
-# Observer
+
+# STRATEGY
+# OBSERVER
